9de29cfdb8e972d82e8bd3d87f531e5aa7b83fc6
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-17 20:34:40 -0700

    fixed error with logo text

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ function runInquirer() {
         const logoSVG = new LogoSVG();
         logoSVG.createText(text, textColor);
         logoSVG.createShape(newShape);
+        console.log(logoSVG.render());
 
         return writeFile('logo.svg', logoSVG.render());
     })
@@ -63,9 +64,3 @@ function runInquirer() {
 }
 
 runInquirer();
-
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
